cc4f2dc7d8681b3c75742d9bd8e04943872532f8
Teddy Neer <teddyneer@Teddys-MacBook-Air.local>
2016-10-20 21:07:24 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple Spotify Search
+
+- In this lab we will work to create an app that searches the Spotify database and displays results.
+- Step 1: Navigate over to the [Spotify API Documentation](https://developer.spotify.com/web-api/) and find the endpoint to "Search for an item".
+- Step 2: Open up app.js and change the "endpoint" variable to the endpoint you found in the documentation.
+- Step 3: Add a click handler for the "Search" button on the index.html document. You can see an [example here](https://developer.mozilla.org/en-US/docs/Mozilla/Tech/XUL/Attribute/onclick). The function name you need to call is performSearch().
+- Step 4: Test out your app by searching for a song.
---
+++ app.js
@@ -0,0 +1,38 @@
+var endpoint = "https://api.spotify.com/v1/search";
+var performSearch;
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var songTemplateSource = document
+        .getElementById("song-template")
+        .innerHTML;
+
+    var songTemplate = Handlebars.compile(songTemplateSource);
+
+    performSearch = function() {
+        var searchQuery = document
+            .getElementById("search-query")
+            .value;
+
+        var oReq = new XMLHttpRequest();
+
+        oReq.addEventListener("load", function() {
+            document
+                .getElementById("search-query")
+                .value = "";
+
+            var tracks = JSON.parse(this.responseText).tracks.items;
+
+            for (var i = 0; i < tracks.length; i++) {
+                document
+                    .getElementById("song-container")
+                    .innerHTML += songTemplate(tracks[i]);
+            }
+        });
+
+        oReq.open("GET", endpoint + "?type=track&q=" + searchQuery);
+
+        oReq.send();
+    };
+
+});
